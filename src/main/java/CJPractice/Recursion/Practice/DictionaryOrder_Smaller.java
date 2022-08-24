856471bae067cd4f17490af0cb1dd4253a6a2c09
srinivaschakrapani87@gmail.com <Nagaratnam@1>
2022-08-24 14:28:37 +0530

    String permutation using recursion

---
+++ src/main/java/CJPractice/Recursion/Practice/DictionaryOrder_Smaller.java
@@ -0,0 +1,61 @@
+package CJPractice.Recursion.Practice;
+/*
+Input Format
+Single line input containing a string
+
+
+Constraints
+Length of string <= 25
+
+
+Output Format
+Display all the words which are in dictionary order smaller than the string entered in a new line each.
+The output strings must be sorted.
+
+
+Sample Input
+cab
+Sample Output
+abc
+acb
+bac
+bca
+Explanation
+The possible permutations of string "cab" are "abc" , "acb" ,"bac" , "bca" , "cab" and "cba" .
+Only four of them are lexicographically less than "cab". We print them in lexicographical order.
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DictionaryOrder_Smaller {
+    static ArrayList<String> al = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        printPermutation("cab", "", "cab");
+        Collections.sort(al);
+//        System.out.println(al);
+        for (String x : al){
+            System.out.println(x);
+        }
+    }
+
+    public static void printPermutation(String inp_str, String path_to_print, String inp_str_orig) {
+        if (inp_str.isEmpty()) {
+            if (path_to_print.compareTo(inp_str_orig) < 0) {
+//                System.out.println(path_to_print);
+                al.add(path_to_print);
+            }
+            return;
+        }
+
+        String to_add = inp_str.substring(0, 1);
+        String rem_str = inp_str.substring(1);
+        for (int i = 0; i <= path_to_print.length(); i++) {
+            printPermutation(rem_str, path_to_print.substring(0, i) + to_add + path_to_print.substring(i, path_to_print.length()), inp_str_orig);
+
+        }
+
+
+    }
+
+}
